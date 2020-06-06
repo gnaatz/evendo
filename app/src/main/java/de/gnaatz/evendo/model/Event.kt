0a58630e3921793d176159911b61a3e6cbf18554
gnaatz <dev@gnaatz.de>
2020-06-06 17:40:48 +0200

    Improve CreateEvent

--- app/src/main/java/de/gnaatz/evendo/model/Event.kt
+++ app/src/main/java/de/gnaatz/evendo/model/Event.kt
@@ -3,8 +3,8 @@ package de.gnaatz.evendo.model
 class Event(var title: String, var description: String, var hour: Int, var minute: Int) {
     private var todos: ArrayList<Todo> = ArrayList()
 
-    fun addTodo(todo: Todo) {
-        todos.add(todo)
+    fun createTodo(title: String) {
+        todos.add(Todo(title))
         //TODO: Update remote
     }
 
